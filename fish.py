c5102122b391060c85eb71ee5b7e6850545f891a
mdkragon <jiali.mdk@gmail.com>
2016-04-23 16:14:10 -0700

    changed the fish.py

--- fish.py
+++ fish.py
@@ -6,12 +6,12 @@ def header(csvfile):
     afile = pd.read_csv(csvfile)
     return set(afile.columns.values)
 
+
 if __name__ == "__main__":
     name = set()
-    base_path = 'C:\Users\Emily Zhai\Desktop\\'
+    base_path = 'C:\Users\jialis\Documents\\fish_data\\'
     for filename in glob.glob(os.path.join(base_path, '*.csv')):
         print "working on: " + filename
         name = name | header(filename)
 
-    print name
-
+    pd.Series(list(name)).to_csv('C:\Users\jialis\Documents\\fish_data\\rawrmooquack.csv')
